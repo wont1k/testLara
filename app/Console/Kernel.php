c0d28653194d10f7fa44eac562803b53cf20a885
Глеб <61438595+wont1k@users.noreply.github.com>
2025-06-24 10:59:18 +0300

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -7,20 +7,16 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 class Kernel extends ConsoleKernel
 {
     /**
-     * Регистрация команд приложения
      *
      * @var array
      */
     protected $commands = [
-        \App\Console\Commands\ListIntervalsCommand::class, // Добавьте эту строку
+        \App\Console\Commands\ListIntervalsCommand::class,
     ];
 
-    /**
-     * Определение расписания команд
-     */
+ 
     protected function schedule(Schedule $schedule)
     {
-        // ...
     }
 
     /**
